1f5e87d2f2090877c5305cd90307ba3ea088d3fc
Hessel van der Molen <hvandermolen@dexels.com>
2022-08-02 14:32:34 +0200

    qn9090: sdk 2.6.5

---
+++ mcux/mcux-sdk/devices/QN9090/soc/mcux_zephyr.c
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2021-2022, sendrato.com
+ * All rights reserved.
+ *
+ * \file
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "mcux_zephyr.h"
+#include <devicetree.h>
+
+#if (defined(gSupportBle) && (gSupportBle == 1))
+
+uint32_t _scratch_buf_start = (DT_REG_ADDR(DT_NODELABEL(sram0)));
+uint32_t _scratch_buf_end   = (DT_REG_ADDR(DT_NODELABEL(sram0)) + 1024);
+
+#endif
---
+++ mcux/mcux-sdk/devices/QN9090/soc/mcux_zephyr.h
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2021-2022, sendrato.com
+ * All rights reserved.
+ *
+ * \file
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _MCUX_ZEPHYR_CONFIG_H_
+#define _MCUX_ZEPHYR_CONFIG_H_
+
+/* GAP role */
+#if defined(CONFIG_BT_CENTRAL)
+#define CFG_CENTRAL
+#endif
+
+#if defined(CONFIG_BT_PERIPHERAL)
+#define CFG_PERIPHERAL
+#endif
+
+#if defined(CONFIG_BT_BROADCASTER)
+#define CFG_BROADCASTER
+#endif
+
+#if defined(CONFIG_BT_OBSERVER)
+#define CFG_OBSERVER
+#endif
+
+/* Max number of connections */
+#ifndef CONFIG_BT_MAX_CONN
+#define gAppMaxConnections_c (1)
+#else
+#define gAppMaxConnections_c (CONFIG_BT_MAX_CONN)
+#endif
+
+#define CONFIG_USE_FIXED_MAC_ADDR 1
+
+#endif /* _MCUX_ZEPHYR_CONFIG_H_ */
