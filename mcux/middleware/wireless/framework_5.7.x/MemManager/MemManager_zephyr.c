fa77a1fdd0d6d0333ea1f99b1bb39bd04654ac4d
Hessel van der Molen <hvandermolen@dexels.com>
2022-08-02 14:32:34 +0200

    qn9090: sdk 2.6.5

---
+++ mcux/middleware/wireless/framework_5.7.x/MemManager/MemManager_zephyr.c
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * Copyright 2021 Sendrato - https://sendrato.com
+ * All rights reserved.
+ *
+ * \file
+ *
+ * This is the source file for the Memory Manager interface.
+ * It is a subset of the MCUXpresso SDK and contains the minimum required
+ * functionality to support Zephyr.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "MemManager.h"
+#include <zephyr.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(nxp_mem, LOG_LEVEL_NONE);
+
+/**
+ * @brief Bytes on Zephyr-heap to be allocated for MemManager
+ */
+K_HEAP_DEFINE(_memHeap, CONFIG_MCUX_MIDDLEWARE_MEMMANAGER_HEAP_SIZE);
+
+/**
+ * @brief   This function initializes the message module private variables.
+ *          Must be called at boot time, or if device is reset.
+ *
+ * @return  MEM_SUCCESS_c if init is successful. (It's always successful).
+*/
+memStatus_t MEM_Init(void)
+{
+    return MEM_SUCCESS_c;
+}
+
+
+/**
+ * @brief  Frees the given buffer.
+ *
+ * @param[in] buffer   Pointer to memory block to free
+ * @return             MEM_SUCCESS_c if free is successful,
+ *                     MEM_ALLOC_ERROR_c otherwise
+ */
+memStatus_t MEM_BufferFree(void* buffer)
+{
+    if( buffer == NULL ) {
+        return MEM_FREE_ERROR_c;
+    }
+    LOG_INF("F:%p", buffer);
+
+    k_heap_free(&_memHeap, buffer);
+    return MEM_SUCCESS_c;
+}
+
+
+/**
+ * @brief   Allocate a block from the memory pools. The function uses the
+ *          numBytes argument to look up a pool with adequate block sizes.
+ *
+ * @param[in]  numBytes  Size of buffer to allocate
+ * @param[in]  poolId    <not used>
+ * @param[in]  pCaller   <not used>
+ * @return               Pointer to the allocated buffer, NULL if failed.
+ */
+void* MEM_BufferAllocWithId(uint32_t numBytes, uint8_t poolId, void *pCaller)
+{
+    ARG_UNUSED(poolId);
+    ARG_UNUSED(pCaller);
+    void* ptr = k_heap_alloc(&_memHeap, numBytes, K_NO_WAIT);
+    LOG_INF("A:%p [%d]", ptr, numBytes);
+    return ptr;
+}
---
+++ mcux/middleware/wireless/framework_5.7.x/RNG/Interface/RNG_Interface.h
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2019 NXP
+ * Copyright 2021 Sendrato B.V. - https://sendrato.com
+ * All rights reserved.
+ *
+ * \file
+ *
+ * It is a subset of the MCUXpresso SDK and contains the minimum required
+ * functionality to support Zephyr.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _RNG_INTERFACE_H_
+#define _RNG_INTERFACE_H_
+
+#include "EmbeddedTypes.h"
+
+/**
+ * @brief Return status of RNG functions
+ */
+#define gRngSuccess_d       (0x00)
+#define gRngInternalError_d (0x01)
+#define gRngNullPointer_d   (0x80)
+#define gRngMaxRequests_d   (100000)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*! *********************************************************************************
+* \brief  Generates a 32-bit statistically random number
+*         No random number will be generated if the RNG was not initialized
+*         or an error occurs.
+*
+* \param[out]  pRandomNo  Pointer to location where the value will be stored
+*
+********************************************************************************** */
+void RNG_GetRandomNo(uint32_t* pRandomNo);
+
+/**
+ * @brief  Returns a random 32 bit word
+ *
+ * A number is read from the HW RNG module. If the HW fails, the SW PRNG is
+ * used as backup.
+ *
+ * @param[out] pRandomNo    Pointer to location where the value will be stored
+ * @return                  Status of the RNG initialization procedure.
+ */
+uint8_t RNG_HwGetRandomNo(uint32_t* pRandomNo);
+
+/**
+ * @brief  Returns a random number between the specified min and max values.
+ *
+ * @param[in] u32Min  minimum value
+ * @param[in] u32Max  maximum value
+ * @return            random number
+ */
+uint32_t RND_u32GetRand(uint32_t u32Min, uint32_t u32Max);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RNG_INTERFACE_H_ */
